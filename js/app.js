5b95fbc168d326c90e214fdb376daf677c99f35a
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2023-01-07 23:15:17 -0600

    implemented bootstrap to make site mobile-friendly

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
-const splash = document.querySelector('.splash');
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    setTimeout(() => {
-        splash.classList.add('display-none');
-    }, 2500);
-})
-
-const toggle = document.getElementById('theme-control'), text = document.querySelector('.b-site--theme-text'), moonSVG = document.querySelector('.b-icon--moon'), sunSVG = document.querySelector('.b-icon--sun'); function themeOff() { document.body.classList.add('t-dark'); sunSVG.style.display = 'inline'; moonSVG.style.display = 'none'; text.innerText = 'too dark?' }
-if (localStorage.getItem('t-dark')) { themeOff(); } else { moonSVG.style.display = 'inline'; }
-toggle.addEventListener('click', function (e) {
-    e.preventDefault(); if (document.body.classList.contains('t-dark')) {
-        document.body.classList.remove('t-dark'); moonSVG.style.display = 'inline'; sunSVG.style.display = 'none'; text.innerText = 'too bright?'
-        localStorage.removeItem('t-dark')
-    } else { themeOff(); localStorage.setItem('t-dark', true) }
-})
+window.onscroll = function () {
+    var top = window.scrollY;
+    if (top >= 100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
